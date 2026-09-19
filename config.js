5ea70975f9aadb1e12c675aad833c6758cdeeaf8
medvi0706 <medvi0706@gmail.com>
2026-09-18 21:44:41 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Pega aquí la URL de tu implementación de Apps Script (termina en /exec).
-const API_URL = "AKfycbzFAOAyqzMWQmwMd_NnAvmZ3kcCtZbZvmnfKLcqi7kimoshgNT00GGTCTEzb4c7TftCJw";
+const API_URL = "https://script.google.com/macros/s/AKfycbzFAOAyqzMWQmwMd_NnAvmZ3kcCtZbZvmnfKLcqi7kimoshgNT00GGTCTEzb4c7TftCJw/exec";
